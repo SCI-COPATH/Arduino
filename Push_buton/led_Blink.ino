2113059ba04027c5c188d7304de67dce76a1611d
Swathy Prasannan <swathyprasanakumar@gmail.com>
2021-06-22 00:58:09 +0530

    Blinking both leds simultaneously

--- Push_buton/led_Blink.ino
+++ Push_buton/led_Blink.ino
@@ -1,24 +1,38 @@
 int led_1=5;
 int led_2=8;
-int delay1=1000;
+int delay1=500;
 int delay2=3000;
-int count=delay2/delay1;
+int count=delay2/delay1;   //1000*3=3000... so 3 times loop
 void setup()
 {
   pinMode(led_1,OUTPUT);
-  pinMode(led_2,OUTPUT); 
+  pinMode(led_2,OUTPUT);
+  //Serial.begin(9600); 
 }
 
 void loop()
 { 
+  //Serial.print(count);
+  
     digitalWrite(led_2,HIGH);
-    for(int i=0; i<count;i++)
+    
+    for(int i=0; i<count/2;i++)   //two times delay so /2 
     {
+      //Serial.println("Entered loop ");
     digitalWrite(led_1,HIGH);
     delay(delay1);
     digitalWrite(led_1,LOW);
+    delay(delay1);
     }
+    
     digitalWrite(led_2,LOW);
-    delay(delay2);
+    
+    for(int i=0;i<count/2;i++) //this will actuaklly create a delay of three sec
+    {
+     digitalWrite(led_1,HIGH); 
+     delay(delay1);
+     digitalWrite(led_1,LOW);
+      delay(delay1);
+    }
   
 }
