1ac3a0476fa7fb52f180c1fda28a1a8b1edb14d8
Swathy Prasannan <swathyprasanakumar@gmail.com>
2021-06-27 19:44:31 +0530

    Updated with two separation in delay  ->  also both work is done simultaneously

--- Push_buton/led_blink_2.ino
+++ Push_buton/led_blink_2.ino
@@ -2,7 +2,11 @@
 #define led_2 8
 #define push_but 2
 int prev=0;
-
+int a;
+int delay1=3000;
+int fac=delay1/1000;
+int delay_fac=delay1/fac;   //3 times loop , so both process appears to run simultaneously
+  
 //FIRST LED BLINK ALWAYS
 // -> SECOND LED ON WHEN BUTTON PRESSED
 // -> SECOND LED OFF WHEN AGAIN BUTTON PRESED
@@ -18,14 +22,92 @@ void setup()
 
 void loop()
 {
-  digitalWrite(led_1,HIGH);
-  delay(1000);
-  int a=digitalRead(push_but);
-  Serial.println("push button working ");
-  Serial.print(a);
+  
+  
+//SECOND LED ON ONLY WHEN BUTTON PRESSED AND OFF WHEN AGAIN BUTTON PRESSED
+  if(delay1>=1000)
+  {      
+    digitalWrite(led_1,HIGH);
+        
+        for(int i=0;i<fac;i++)
+        {
+          a=digitalRead(push_but);
+          Serial.println("push button working ");
+          Serial.println(a);
+
+             if(a==1)
+              {
+                if(prev==1)
+                {
+                  digitalWrite(led_2,LOW);
+                  prev=0;
+                }
+                else if(prev==0)
+                {
+                  digitalWrite(led_2,HIGH);
+                  prev=1;
+                }
+              }
+              delay(delay_fac);
+        }
+  digitalWrite(led_1,LOW);
+
+      for(int i=0;i<fac;i++)
+        {
+          a=digitalRead(push_but);
+          Serial.println("push button working ");
+          Serial.println(a);
+
+             if(a==1)
+              {
+                if(prev==1)
+                {
+                  digitalWrite(led_2,LOW);
+                  prev=0;
+                }
+                else if(prev==0)
+                {
+                  digitalWrite(led_2,HIGH);
+                  prev=1;
+                }
+              }
+              delay(delay_fac);
+        }
+  }
 
 
-  //first one not working ,problem ->delay in reading, not entering inside 'if' ie remaining in only high state always
+  else
+    {
+      digitalWrite(led_1,HIGH);
+      delay(delay1);
+    a=digitalRead(push_but);
+    Serial.println("push button working ");
+    Serial.println(a);
+          if(a==1)
+              {
+                if(prev==1)
+                {
+                  digitalWrite(led_2,LOW);
+                  prev=0;
+                }
+                else if(prev==0)
+                {
+                  digitalWrite(led_2,HIGH);
+                  prev=1;
+                }
+              }
+
+
+      digitalWrite(led_1,LOW);
+       delay(delay1);
+  
+    }
+  
+}
+
+
+
+//first one not working ,problem ->delay in reading, not entering inside 'if' ie remaining in only high state always
             //  if(a==1)
             //  {
             //    digitalWrite(led_2,HIGH);
@@ -48,27 +130,3 @@ void loop()
             //      { digitalWrite(led_2,LOW);
             //    }
             
-
-
-
-
-//SECOND LED ON ONLY WHEN BUTTON PRESSED AND OFF WHEN AGAIN BUTTON PRESSED
-
-             if(a==1)
-              {
-                if(prev==1)
-                {
-                  digitalWrite(led_2,LOW);
-                  prev=0;
-                }
-                else if(prev==0)
-                {
-                  digitalWrite(led_2,HIGH);
-                  prev=1;
-                }
-              }
-
-  
-  digitalWrite(led_1,LOW);
-  delay(250);
-}
