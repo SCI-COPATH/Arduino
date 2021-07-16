5fd8e46d68ca392c35fca73c9fc8881ac5fbb4a1
Swathy Prasannan <swathyprasanakumar@gmail.com>
2021-07-17 01:03:53 +0530

    both working as per condition...

--- Push_buton/short_long_press.ino
+++ Push_buton/short_long_press.ino
@@ -1,3 +1,11 @@
+//PROBLEM STATEMENT
+//      ->RED ON/OFF WHWN SINGLE TIME PRESS   
+//      ->BLUE LED ON/OFF WHEN LONG PRESS (w8 not more than 500 msec) 
+
+//works only for long press not more than 1 sec -> issue -blinking 
+
+
+
 int prev =0;
 #define fac 250
 #define red 5
@@ -16,31 +24,29 @@ void setup()
 void loop()
 {
   //state=0;
-  demp=0;
-   state = digitalRead(push);
-   demp=demp+state;// the count the delay causwesd
+   demp=0;
   
-  if(state==1 && demp==1) // condition for red led on off ->for single press
-  {
+  // the count the delay causwesd
+   demp = push_read();
+   if(state==1 && demp==1) // condition for red led on off ->for single press
+    {
          
-    if(prev==0)
-    {
-  digitalWrite(red, HIGH);
-  delay(fac); 
-  prev= 1;
-    }
+     if(prev==0)
+      {
+        digitalWrite(red, HIGH);
+        delay(fac); 
+        prev= 1;
+      }
     
-    else if(prev==1)
-    {
-      digitalWrite(red, LOW);
-      delay(fac); 
-      prev= 0;
+     else if(prev==1)
+      {
+        digitalWrite(red, LOW);
+        delay(fac); 
+        prev= 0;
+      }
+      //demp=0;
+      demp = push_read();
     }
-    //demp=0;
-    state = digitalRead(push);
-   demp=demp+state;
-    
-  }
   
  //problem exit when long press continoous blinking of red occurs
  
@@ -49,26 +55,31 @@ void loop()
      
   //  }
   
-   if(state==1 && demp>1)   // for long press
-  {
-    //digitalWrite(red, LOW);
-    if(pre==0)
-    {
-  digitalWrite(blue, HIGH);
-  delay(fac); 
-  pre= 1;
-    }
+    if(state==1 && demp>1)   // for long press
+      {
+        //digitalWrite(red, LOW);
+        if(pre==0)
+        {
+          digitalWrite(blue, HIGH);
+          delay(fac); 
+          pre= 1;
+        }
     
     else if(pre==1)
-    {
-      digitalWrite(blue, LOW);
-      delay(fac); 
-      pre= 0;
-    }
+      {
+        digitalWrite(blue, LOW);
+        delay(fac); 
+        pre= 0;
+      }
     
-  }
-  //state = digitalRead(push);
-   //demp=demp+state;
-  //state = digitalRead(push);
+      }
   
+}
+
+// fun to read push buton
+int push_read()
+{
+   state = digitalRead(push);
+   demp=demp+state;
+   return demp;
 }
